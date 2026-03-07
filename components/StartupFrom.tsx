6f1f9e6aef5196da283d30b1eaafdca81e8f47c2
Youssef Saeid <146128402+YoussefSaeidSaleh@users.noreply.github.com>
2026-03-07 21:57:21 +0200

    add useActionState

--- components/StartupFrom.tsx
+++ components/StartupFrom.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useActionState, useState } from "react";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 import MDEditor from "@uiw/react-md-editor";
@@ -10,8 +10,7 @@ import { Send } from "lucide-react";
 const StartupFrom = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
   const [pitch, setPitch] = React.useState("");
-  const isPending = false;
-
+  const [state, formAction, isPending] = useActionState();
   return (
     <form action={() => {}} className="startup-form">
       <div>
