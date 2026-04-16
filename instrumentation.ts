20b4232852722ed21d7bf410c829761a7e9409bc
Youssef Saeid <146128402+YoussefSaeidSaleh@users.noreply.github.com>
2026-04-16 18:01:16 +0200

    fix/ bug

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,9 @@
 import * as Sentry from "@sentry/nextjs";
+import { connection } from "next/server";
 
 export async function register() {
+  await connection();
+
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
   }
